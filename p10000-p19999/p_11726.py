bed7da7a11c7ed66daadbd775a63f233b4bc3270
Suchan Lee <lsc1814@naver.com>
2023-09-07 12:20:39 +0900

    solved 11727 and 11726

--- p10000-p19999/p_11726.py
+++ p10000-p19999/p_11726.py
@@ -3,11 +3,11 @@ import sys
 n = int(sys.stdin.readline().rstrip())
 tile = [0] * 1001
 tile[1] = 1
-tile[2] = 3
+tile[2] = 2
 
 if n < 3:
     print(tile[n])
 else:
     for i in range(3, n + 1):
-        tile[i] = (tile[i - 1] + 2 * tile[i - 2]) % 10007
+        tile[i] = (tile[i - 1] + tile[i - 2]) % 10007
     print(tile[n])
